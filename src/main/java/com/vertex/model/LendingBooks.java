30a586a216af899c224fa4bb1fddc736ba09305f
PiyumiNadeera <dlpnadeera1234@gmail.com>
2023-07-22 10:03:37 +0530

    Add lending books

---
+++ src/main/java/com/vertex/model/LendingBooks.java
@@ -0,0 +1,12 @@
+package com.vertex.model;
+
+public class LendingBooks {
+    String book;
+    int ISBN;
+    int availability;
+
+    public static void lendingBooks(){
+        System.out.println("Lending a book");
+        System.out.println("Enter the ISBN");
+    }
+}
