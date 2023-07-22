99ec66adbe24687d2c59bac16619a1b1309a611b
Pethum Jeewantha <pethumjeewantha1@gmail.com>
2023-07-22 14:12:17 +0530

    remove comment

--- src/main/java/com/vertex/service/BookService.java
+++ src/main/java/com/vertex/service/BookService.java
@@ -7,12 +7,8 @@ import java.util.ArrayList;
 public interface BookService {
      int addBook(Book book);
     Book showBook(long id);
-    //show all employee
     ArrayList<Book> showAllBooks();
-    //show employee based on id
     ArrayList<Book> showAvailableBooks();
-    //update employee
     int updateBook(long id, Book book);
-    //delete employee
     Book deleteBook(long id);
 }
