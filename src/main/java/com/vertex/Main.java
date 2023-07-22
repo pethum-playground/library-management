1597e6891708330151dc24bf30a8bf0b94cc2bd9
PiyumiNadeera <dlpnadeera1234@gmail.com>
2023-07-22 10:46:42 +0530

    Add lending books class

--- src/main/java/com/vertex/Main.java
+++ src/main/java/com/vertex/Main.java
@@ -1,7 +1,15 @@
 package com.vertex;
 
+import com.vertex.model.Book;
+import com.vertex.model.LendingBooks;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("came");
+
+
     }
+
 }
--- src/main/java/com/vertex/model/LendingBooks.java
+++ src/main/java/com/vertex/model/LendingBooks.java
@@ -1,12 +1,50 @@
 package com.vertex.model;
-
 public class LendingBooks {
-    String book;
-    int ISBN;
-    int availability;
+    private String book;
+    private long ISBN;
+    private int availability;
 
-    public static void lendingBooks(){
-        System.out.println("Lending a book");
-        System.out.println("Enter the ISBN");
+    public LendingBooks() {
     }
+
+    public LendingBooks(String book, long ISBN, int availability) {
+        this.book = book;
+        this.ISBN = ISBN;
+        this.availability = availability;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public double getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public String toString() {
+        return "LendingBooks{" +
+                "book='" + book + '\'' +
+                ", ISBN=" + ISBN +
+                ", availability=" + availability +
+                '}';
+    }
+
+
 }
